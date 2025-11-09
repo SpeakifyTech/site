8fc99d442587d303e9febaa7605768dbf0752cdd
Mehul <github@mehapps.com>
2025-11-09 05:46:10 -0500

    random bs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ['@prisma/client', 'prisma'],
+
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.plugins = [...config.plugins, new PrismaPlugin()]
