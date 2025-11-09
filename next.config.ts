e6e9564e9a05943fab3c4d2ad372ecaa98268fbb
Mehul <github@mehapps.com>
2025-11-09 04:12:43 -0500

    Revert "try fix"
    This reverts commit 8473cfcd677cc00cab0891f6aeec7273f95f3cf8.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias["node:os"] = "os";
-    config.resolve.alias["node:fs"] = "fs";
-    config.resolve.alias["node:path"] = "path";
-    return config;
-  }
+  /* config options here */
 };
 
 export default nextConfig;
